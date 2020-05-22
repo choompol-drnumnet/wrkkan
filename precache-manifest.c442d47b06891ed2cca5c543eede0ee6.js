af553cf688a0a188ba1aeb5323f2013029d3f979
choompol-edipaorg <choompol@edipa.org>
2020-05-22 07:46:53 +0700

    Updates

---
+++ precache-manifest.c442d47b06891ed2cca5c543eede0ee6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c424d960d3d53f674fedaddd01405a48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b68ed4f4807ae84b357f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b64420eab4f9ce2b3104",
+    "url": "/static/js/2.b24b0ca1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b24b0ca1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b68ed4f4807ae84b357f",
+    "url": "/static/js/main.d193d269.chunk.js"
+  },
+  {
+    "revision": "63a49f0482c09cbcbabd",
+    "url": "/static/js/runtime-main.cfc8a05f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
